30ea9a3a8e58d7d599e8ccc4817ac5f6ee28dd73
Harsh Bhadauria <97093305+iamharshbhadauria@users.noreply.github.com>
2023-09-01 23:35:33 +0530

    Add files via upload

---
+++ A_Grasshopper_on_a_Line.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define int long long
+
+void solve(){
+    int n;
+    cin >> n;
+
+
+}
+
+signed main(){
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+    auto Harsh_Bhadauria = chrono::high_resolution_clock::now();
+
+    int t;
+    cin >> t;
+    while(t--){
+        solve();
+    }
+}
